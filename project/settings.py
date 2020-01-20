6c0b9de7a4887cd2afb6b4ae5819072ebab272cd
Alexander Neupokoev <admin@armavir.ru>
2020-01-20 22:25:32 +0300

    Change os.getenv for DJANGO_DEBUG to enb.bool. Add to requirments.txt need library

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,17 @@
 import os
 from dotenv import load_dotenv
+from environs import Env
 
 load_dotenv()
+env = Env()
+env.read_env()
+
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DJANGO_DEBUG = os.getenv('DJANGO_DEBUG')
+DJANGO_DEBUG = env.bool('DJANGO_DEBUG')
 DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DATABASES = {
